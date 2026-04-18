a1c484a257f5e89c89a7d619aac7877cf7673faa
jazkhan27 <jasminekhanm786@gmail.com>
2026-04-18 19:36:32 +0530

    Update response_generator.py

--- response_generator.py
+++ response_generator.py
@@ -16,3 +16,11 @@ class ResponseGenerator:
         elif intent == "count_students":
             # the result is in the first row, first col
             total = int(df.iloc[0, 0])
+            return f"There are a total of {total} students."
+            
+        elif intent == "top_performers":
+            if count == 1:
+                return f"Here is the top performer from the class:"
+            else:
+                return f"Here are the top {count} performers based on their marks:"
+                
